d8e1f7015430d6b03b8811ff50ed29aae0d677b8
Manoel Lobo <manoel@ufcg.edu.br>
2017-03-02 17:02:38 -0300

    Notificações utilizáveis

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { NotificationPage } from '../pages/notification/notification';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    NotificationPage
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -14,7 +16,8 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    NotificationPage
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
